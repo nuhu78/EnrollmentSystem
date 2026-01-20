abf9baf2ab3351016320fa2b263f98aac2dd79cf
nuhu78 <22-48664-3@student.aiub.edu>
2026-01-20 13:20:22 +0600

    remove some unnecessary file

--- DAL/Migrations/20260120063608_TestChec2.Designer.cs
+++ DAL/Migrations/20260120063608_TestChec2.Designer.cs
@@ -3,6 +3,7 @@ using DAL.EF;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace DAL.Migrations
 {
     [DbContext(typeof(UMSContext))]
-    partial class UMSContextModelSnapshot : ModelSnapshot
+    [Migration("20260120063608_TestChec2")]
+    partial class TestChec2
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
